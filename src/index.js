af6f3079d67a62d7fe25f4a1060883fed913b826
ndrafke <nickdrafke@gmail.com>
2021-08-18 14:01:31 -0500

    links and css adjustments

--- src/index.js
+++ src/index.js
@@ -23,19 +23,31 @@ function Iluvdolfinz (){
             <div id="full">
                 <Navigation />
             <section id="home">
-                <h5>New album, "Brain Slop" now available to stream!</h5>
+                <h6>Full-length album, "Brain Slop" now available to stream!</h6>
             <div id="album-container">
-           <a href="https://open.spotify.com/album/6lhLVV61rnepb4Kj5gJRej?si=8kGZLk5XQcGGysyq7tya1w&amp;dl_branch=1" target="_blank"> <img id="album-cover" alt="iluvdolfinz brain slop album art" src="https://i.imgur.com/07If8aZ.jpg"></img></a>
+           <a href="https://open.spotify.com/album/6lhLVV61rnepb4Kj5gJRej?si=8kGZLk5XQcGGysyq7tya1w&amp;dl_branch=1" target="_blank"> <img id="album-cover" alt="iluvdolfinz brain slop album art" src="https://i.imgur.com/07If8aZ.jpg"></img>
+           </a>
            </div>
-           <p>Click to open in Spotify</p>
+           <a href="https://open.spotify.com/album/6lhLVV61rnepb4Kj5gJRej?si=8kGZLk5XQcGGysyq7tya1w&amp;dl_branch=1" target="_blank"><p>Click to open in Spotify</p></a>
+      
             </section>
 
             <section id="listen">
-            <iframe src="https://open.spotify.com/embed/album/6lhLVV61rnepb4Kj5gJRej" width="90%" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+              
+            <iframe src="https://open.spotify.com/embed/album/6lhLVV61rnepb4Kj5gJRej" width="90%" height="460" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            
+            <hr></hr>
             <h6>Other Streaming Options:</h6>
             <div className="stream-links">
-                <a href="https://www.youtube.com/playlist?list=PL5lpvt-ta3jAdnBh5CHrTJR31QKXjxkW5" target="_blank">Youtube</a>
-                <a href="https://music.apple.com/us/album/brain-slop/1556117442" target="_blank"> &rarr;Apple Music</a>  
+              <div>
+                <a href="https://www.youtube.com/playlist?list=PL5lpvt-ta3jAdnBh5CHrTJR31QKXjxkW5" target="_blank"><img id="yt-logo" src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Emblem.png" alt="youtube logo"></img></a>
+                </div>
+                <div>
+                <a href="https://music.apple.com/us/album/brain-slop/1556117442" target="_blank"><img id="itunes-logo" src="https://www.pinclipart.com/picdir/middle/572-5729091_vector-apple-music-logo-png-clipart.png" alt="apple music logo"></img></a>
+                </div>
+                <div>
+                <a href="https://music.amazon.com/artists/B08XWLSKY8/iluvdolfinz" target="_blank"><img id="amazon-logo" src="https://image.pngaaa.com/951/604951-small.png" alt="amazon music logo"></img></a> 
+                </div> 
             </div>
             </section>
             <section id="video">
@@ -43,10 +55,32 @@ function Iluvdolfinz (){
                 <p>coming soon</p>
             </section>
             <div id="about">
+              <div> </div>
+              <div> </div>
                 <h5>WTF is iluvdolfinz?</h5>
                 <p className="bio">iluvdolfinz formed spontaneously in 2006, a result of several quantum fluctuations. Three unreleased albums were produced in the following years, with some songs making appearances online via MySpace, YouTube, and PureVolume. After lying dormant for many years, iluvdolfinz re-emerged with a new full-length album, 'Brain Slop', which was produced, published and released via online streaming platforms in 2021. iluvdolfinz is influenced by chaos, impulse, time-travel, videotape, motion, disorder, and delusions of grandeur. The name "iluvdolfinz" was created as a joke that stuck, and is in reference to nothing. </p>
+                <div> </div>
+                <h6>Follow iluvdolfinz:</h6>
+<div className="button-box">
+  <div>
+  <a href="https://twitter.com/iluvdolfinz_?ref_src=twsrc%5Etfw" target="_blank"><img id="twitter-logo" src="https://www.vhv.rs/dpng/d/211-2110922_transparent-background-twitter-logo-hd-png-download.png" alt="twitter logo"></img></a>
+  </div>
+  <div>
+  <a href="https://www.instagram.com/iluvdolfinz/" target="_blank">
+   <img id="insta-logo" src="https://logos-world.net/wp-content/uploads/2020/04/Instagram-icon-Logo-2016-present.png" alt="instagram logo"></img>
+</a>
+</div>
+<div>
+<a href="https://www.youtube.com/channel/UC_UQyIv879DV-Kdu3g4dRxQ?sub_confirmation=1" target="_blank">
+<img id="yt-logo" src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Emblem.png" alt="youtube logo"></img></a>
+</div>
+<div>
+<a href="https://www.facebook.com/iluvdolfinz" target="_blank"><img id="fb-logo" src="https://i.pinimg.com/564x/63/a2/31/63a231592efca78f2bcbc02267eb37be.jpg" alt="facebook logo"></img></a>
+</div>
+  </div>
+</div>
             </div>
-            </div>
+            
         )
     }
 
