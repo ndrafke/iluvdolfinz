f07c812a53a999a798f56e344b718309bfda45b6
ndrafke <nickdrafke@gmail.com>
2021-10-04 12:56:29 -0500

    home page style changes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import MountEffect from './components/MountEffect.js';
 
 
 function Iluvdolfinz (){
-
+  //For mountEffect:
    const [isLoading, setIsLoading] = useState(true);
 
    useEffect(() => {
@@ -23,13 +23,31 @@ function Iluvdolfinz (){
             <div id="full">
                 <Navigation />
             <section id="home">
+              <div className="d-flex align-items-center justify-content-center" style={{height: "15vh", width: "95vw"}}>
                 <h6>Iluvdolfinz' full-length album, "Brain Slop" now available to stream!</h6>
-            <div id="album-container">
-           <a href="https://open.spotify.com/album/6lhLVV61rnepb4Kj5gJRej?si=8kGZLk5XQcGGysyq7tya1w&amp;dl_branch=1" target="_blank"> <img id="album-cover" alt="iluvdolfinz brain slop album art" src="https://i.imgur.com/07If8aZ.jpg?2"></img>
+                </div>
+                <div className="home-box d-flex flex-row align-items-center justify-content-center" style={{height: "70vh", width: "85vw"}}>
+                  <div className="d-flex flex-column align-items-center justify-content-center" style={{height: "100%", width:"75%"}}>
+            
+            <div id="album-cover" className="home-box d-flex flex-column align-items-center justify-content-center" style={{height: "75%", width: "100%"}}>
+              
+           <a href="https://open.spotify.com/album/6lhLVV61rnepb4Kj5gJRej?si=8kGZLk5XQcGGysyq7tya1w&amp;dl_branch=1" target="_blank" style={{height: "75%", width: "100%"}}> 
            </a>
+        
            </div>
-           <a href="https://open.spotify.com/album/6lhLVV61rnepb4Kj5gJRej?si=8kGZLk5XQcGGysyq7tya1w&amp;dl_branch=1" target="_blank"><p>Click to open in Spotify</p></a>
-      
+           <div className="home-box d-flex flex-column align-items-center justify-content-center" style={{height: "25%", width: "100%"}}>
+           </div>
+           </div>
+           <div className="d-flex flex-column align-items-center justify-content-center" style={{height: "100%", width: "25%"}}>
+             
+           <div className="home-box d-flex flex-column align-items-center justify-content-center" style={{height: "25%", width: "100%"}}>
+           <a href="https://open.spotify.com/album/6lhLVV61rnepb4Kj5gJRej?si=8kGZLk5XQcGGysyq7tya1w&amp;dl_branch=1" target="_blank"><p style={{textAlign: "center"}}>Click to open in Spotify</p></a>
+           </div>
+           <div className="home-box d-flex flex-column align-items-center justify-content-center" style={{height: "75%", width: "100%"}}>
+               
+             </div>
+           </div>
+           </div>
             </section>
 
             <section id="listen">
